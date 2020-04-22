f4dd05ae36e3c08f052dd8b2f0554d703f676f5e
aoifeofloinn <c18331503@mydit.ie>
2020-04-22 18:42:48 +0100

    Test

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,43 +1,64 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
-	
+	ArrayList<Task> tasks = new ArrayList<Task>();
+
+	public void loadTasks()
+	{
+		Table table = loadTable("tasks.csv", "header");
+		for(TableRow row:table.rows())
+		{
+			Task t = new Task(row);
+			tasks.add(t);
+		}
+	}
+
 	public void settings()
 	{
 		size(800, 600);
 	}
 
-	public void loadTasks()
-	{
-		
-	}
-
 	public void printTasks()
 	{
-		
+		for(Task t:tasks)
+		{
+			System.out.println(t);
+		}
+	}
+
+	public void displayTasks()
+	{
+
 	}
 	
 	public void mousePressed()
 	{
-		println("Mouse pressed");	
+		println("Mouse pressed");
+		displayTasks();	
 	}
 
 	public void mouseDragged()
 	{
 		println("Mouse dragged");
+		displayTasks();
 	}
-
 	
-	
-	public void setup() 
+	public void setup()
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
 	{			
 		background(0);
+		displayTasks();
 	}
 }
---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,51 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Task
+{
+    private String Task;
+    private Float Start;
+    private Float End;
+
+    public Task(String Task, Float Start, Float End)
+    {
+       this.Task = Task;
+       this.Start = Start;
+       this.End = End; 
+    }
+
+    public Task(TableRow tr)
+    {
+        this(tr.getString("Task"), tr.getFloat("Start"), tr.getFloat("End"));
+    }
+
+    public String getTask() {
+        return Task;
+    }
+
+    public void setTask(String task) {
+        Task = task;
+    }
+
+    public Float getStart() {
+        return Start;
+    }
+
+    public void setStart(Float start) {
+        Start = start;
+    }
+
+    public Float getEnd() {
+        return End;
+    }
+
+    public void setEnd(Float end) {
+        End = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [End=" + End + ", Start=" + Start + ", Task=" + Task + "]";
+    }
+}
